9d928727f821d467afce893f90a307d051d513c4
Leon <39353992+lhoogenraad@users.noreply.github.com>
2020-08-30 20:09:51 +1200

    more comments

--- www/display/index.php
+++ www/display/index.php
@@ -14,6 +14,7 @@
 <tr><th>Paper Code</th><th>Exam Year</th><th>Exam Question</th><th>Answer</th><th>University Username (optional)</th></tr>
 <?php
 
+  // These vars are used to connect to the mysql db automatically (basically makes code cleaner)
 $db_host = '192.168.2.12';
 $db_name = 'examanswers';
 $db_user = 'webuser';
@@ -23,8 +24,10 @@ $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
 
 $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
 
+
 $q = $pdo->query("SELECT * FROM answers");
 
+// While we have data left from our select query, output that data to the table
 while($row = $q->fetch()){
   echo "<tr><td>".$row["code"]."</td><td>".$row["year"]."</td><td>".$row["question"]."</td><td>".$row["answer"]."</td><td>".$row["username"]."</td></tr>\n";
 }
