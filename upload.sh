f16bc2ac7245bca33305167a8be78d55a37275df
Leon Hoogenraad <le.o.n@outlook.com>
2020-08-30 13:15:49 +1200

    created upload.sh config file

--- upload.sh
+++ upload.sh
@@ -1,1 +1,20 @@
 echo hello, world
+
+apt-get update
+
+# Installing the apache2, mysql, and php pakcages
+apt-get install -y apache2 php libapache2-mod-php php-mysql
+
+
+# Here we change the VM's web server config
+# to use the one from the shared folder
+cp /vagrant/upload.conf /etc/apache2/sites-available/
+
+# Activating our website config
+a2ensite upload
+
+# Disabling the default website apache2 gives
+a2dissite 000-default
+
+# Reloading the webserver config to affect our changes
+service apache2 reload
