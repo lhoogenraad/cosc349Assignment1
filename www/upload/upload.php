32dc5ffe7157fa2e00e0d38d94401f2d3b193cd8
Leon Hoogenraad <le.o.n@outlook.com>
2020-08-30 13:23:17 +1200

    Created base HTML page for upload page

---
+++ www/upload/upload.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+<head>
+  <link rel='icon' type='image/x-icon' href='/favicon.ico' />
+  <link rel='stylesheet' href='upload.css'>
+  <title>Upload an Answer</title>
+</head>
+
+<body>
+
+<h1>Upload an Answer</h1>
+
+</table>
+
+</body>
+</html>
+
+
+
