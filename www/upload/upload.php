ed9d7701b4d9081939313ee74478dae6345745b5
Leon <39353992+lhoogenraad@users.noreply.github.com>
2020-08-30 14:16:59 +1200

    Update upload.php

--- www/upload/upload.php
+++ www/upload/upload.php
@@ -1,23 +1,23 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
-<html>
-<head>
-  <link rel='icon' type='image/x-icon' href='/favicon.ico' />
-  <link rel='stylesheet' href='upload.css'>
-  <title>Upload an Answer</title>
-</head>
+  <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+ <html>
+    <head>
+      <link rel='icon' type='image/x-icon' href='/favicon.ico' />
+      <link rel='stylesheet' href='upload.css'>
+      <title>Upload an Answer</title>
+    </head>
 
-<body>
+    <body>
 
-<h1>Upload an Answer</h1>
-  <form>
-    <input type="text" name="code" id="code" placeholder="Paper code" required>
-    <input type="text" name="year" id="year" placeholder="Exam year" required>
-    <input type="text" name="question" id="question" placeholder="Question" required>
-    <input type="text" name="answer" id="answer" placeholder="Your answer" required>
-    <input type="text" name="username" id="username" placeholder="Username (optional)">
-  </form>
-<a href="http://127.0.0.1:8080/index.php">Back to Home</a>
-</body>
+    <h1>Upload an Answer</h1>
+      <form>
+        <input type="text" name="code" id="code" placeholder="Paper code" required>
+        <input type="text" name="year" id="year" placeholder="Exam year" required>
+        <input type="text" name="question" id="question" placeholder="Question" required>
+        <input type="text" name="answer" id="answer" placeholder="Your answer" required>
+        <input type="text" name="username" id="username" placeholder="Username (optional)">
+      </form>
+      <a href="http://127.0.0.1:8080/index.php">Back to Home</a>
+    </body>
 </html>
 
 
