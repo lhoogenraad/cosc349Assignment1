822d3a746e52f9f06dce2e68fe74e4140b11e56c
Leon <39353992+lhoogenraad@users.noreply.github.com>
2020-08-30 20:01:07 +1200

    echo comments made

--- display.sh
+++ display.sh
@@ -18,3 +18,12 @@ a2dissite 000-default
 
 # Reloading the webserver config to affect our changes
 service apache2 reload
+
+echo This machine runs a forwaded port on 127.0.0.1:8080
+echo This machine is also running an interface on 192.168.11, which
+echo allows it to access the mysql database server (which runs on 192.168.2.12)
+
+
+echo This machine should now be accepting http requests.
+echo navigate to: 127.0.0.1:8080/index.php to access the exam answers home page.
+
