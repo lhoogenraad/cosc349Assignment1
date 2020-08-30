af59ff1f1384423abbb3a095e49f67be7dfac041
Leon <39353992+lhoogenraad@users.noreply.github.com>
2020-08-30 14:16:19 +1200

    added input form to upload.php

--- www/upload/upload.php
+++ www/upload/upload.php
@@ -9,7 +9,13 @@
 <body>
 
 <h1>Upload an Answer</h1>
-
+  <form>
+    <input type="text" name="code" id="code" placeholder="Paper code" required>
+    <input type="text" name="year" id="year" placeholder="Exam year" required>
+    <input type="text" name="question" id="question" placeholder="Question" required>
+    <input type="text" name="answer" id="answer" placeholder="Your answer" required>
+    <input type="text" name="username" id="username" placeholder="Username (optional)">
+  </form>
 <a href="http://127.0.0.1:8080/index.php">Back to Home</a>
 </body>
 </html>
