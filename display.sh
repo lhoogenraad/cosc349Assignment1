87a429f7dcfc8e26e86d1e92f4f806687df28816
Leon <39353992+lhoogenraad@users.noreply.github.com>
2020-08-31 11:34:39 +1200

    removed brakcets

--- display.sh
+++ display.sh
@@ -19,9 +19,9 @@ a2dissite 000-default
 # Reloading the webserver config to affect our changes
 service apache2 reload
 
-echo This machine runs a forwaded port on 127.0.0.1:8080, which accepts http requests (using apache2 webserver)
+echo This machine runs a forwaded port on 127.0.0.1:8080, which accepts http requests using apache2 webserver
 echo This machine is also running an interface on 192.168.11, which
-echo allows it to access the mysql database server (which runs on 192.168.2.12)
+echo allows it to access the mysql database server which runs on 192.168.2.12
 
 
 echo This machine should now be accepting http requests.
