33cd137c8b510d3e8bf8f7112178e72427bec4a1
Leon <39353992+lhoogenraad@users.noreply.github.com>
2020-08-30 15:27:04 +1200

    Update upload.php

--- www/upload/upload.php
+++ www/upload/upload.php
@@ -8,7 +8,7 @@
 
     <body>
     <h1>Upload an Answer</h1>
-      <form action="upload.php" method="post">
+      <form action="uploadToDB.php" method="post">
         <input type="text" name="code" id="code" placeholder="Paper code" required>
         <input type="text" name="year" id="year" placeholder="Exam year" required>
         <input type="text" name="question" id="question" placeholder="Question" required>
@@ -17,34 +17,6 @@
         <button type="submit" id="submit" value="Submit">Submit</button>
       </form>
       <a href="http://127.0.0.1:8080/index.php">Back to Home</a>
-      
-    <?php
-      $db_host = '192.168.2.12';
-      $db_name = 'examanswers';
-      $db_user = 'webuser';
-      $db_passwd = 'db_pw';
-
-      $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
-
-      $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
-      
-      /*
-        This code uses prepared statements to prevent against sql injections
-      */
-        
-      $sql = "INSERT INTO answer VALUES(?,?,?,?,?)";
-      
-      $stmt = $pdo->prepare($sql);
-        
-      $form = $_POST;
-      $code = $form['code'];
-      $year = $form['year'];
-      $question = $form['question'];
-      $answer = $form['answer'];
-      $username = $form['username'];
-
-      $result = $stmt->execute([$code, $year, $question, $answer, $username]);
-    ?>
    </body>
 </html>
 
